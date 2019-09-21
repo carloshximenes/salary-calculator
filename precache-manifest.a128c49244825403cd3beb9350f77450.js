2860cadded2bc82a30171aaeb5d2950ebf0c5a04
Carlos <carloshximenes@gmail.com>
2019-09-20 23:06:28 -0300

    Updates

---
+++ precache-manifest.a128c49244825403cd3beb9350f77450.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b021533d215447a6b08f8f21d0b6e0ba",
+    "url": "/calculo-salario-liquido/index.html"
+  },
+  {
+    "revision": "54e0cab5e188d6709b36",
+    "url": "/calculo-salario-liquido/static/css/main.dd541e96.chunk.css"
+  },
+  {
+    "revision": "2f2722c6bfb8d4632d44",
+    "url": "/calculo-salario-liquido/static/js/2.b3d2eca1.chunk.js"
+  },
+  {
+    "revision": "54e0cab5e188d6709b36",
+    "url": "/calculo-salario-liquido/static/js/main.3540a7d8.chunk.js"
+  },
+  {
+    "revision": "7b03084fe715bca0472b",
+    "url": "/calculo-salario-liquido/static/js/runtime-main.d96b06fc.js"
+  }
+]);
